a0a9239ee15fd9a1a9cb9446f27cea44198f1ae1
Raul Andrade <raul.andrade21@gmail.com>
2021-01-26 01:17:20 -0300

    Subindo layouts(Login, Register, RegisterGame)

--- app/src/main/java/com/example/desafiofirebase/HomeActivity.kt
+++ app/src/main/java/com/example/desafiofirebase/HomeActivity.kt
@@ -1,11 +1,17 @@
 package com.example.desafiofirebase
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+
+        findViewById<FloatingActionButton>(R.id.fbRegisterGame).setOnClickListener {
+            startActivity(Intent(this, RegisterGameActivity::class.java))
+        }
     }
 }
